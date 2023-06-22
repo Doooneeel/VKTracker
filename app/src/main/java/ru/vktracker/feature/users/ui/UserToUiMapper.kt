d830222f8d4c9a7de113962dcb31dc67670cd608
Doooneeel <daniil.glazkov2002@gmail.com>
2023-06-22 13:49:34 +0700

    Small users refactoring

--- app/src/main/java/ru/vktracker/feature/users/ui/UserToUiMapper.kt
+++ app/src/main/java/ru/vktracker/feature/users/ui/UserToUiMapper.kt
@@ -1,7 +1,7 @@
 package ru.vktracker.feature.users.ui
 
 import ru.vktracker.core.common.IsTracked
-import ru.vktracker.core.common.User
+import ru.vktracker.core.common.user.User
 
 /**
  * @author Danil Glazkov on 02.06.2023, 14:03
